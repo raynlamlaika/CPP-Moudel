a17ec4aef2367bd7e5264a1f5604e30204604198
Rayane Lamlaika <lamlaikarayane@gmail.com>
2025-10-07 10:15:53 +0100

    gh

---
+++ cpp04/ex04e/AMateria.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 09:50:41 by rlamlaik          #+#    #+#             */
+/*   Updated: 2025/10/07 10:01:39 by rlamlaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+
+AMateria::AMateria()
+{
+    this->Type = "default";
+    std::cout << "AMateria: constructor called for type " << this->Type << std::endl;
+}
+AMateria::AMateria(std::string const &type) : Type(type)
+{
+    std::cout << "AMateria: constructor called for type " << this->Type << std::endl;
+}
+
+AMateria::AMateria(const AMateria &other)
+{
+    std::cout << "AMateria: copy constructor called" << std::endl;
+    this->Type = other.Type;
+}
+
+AMateria::~AMateria()
+{
+    std::cout << "AMateria: destructor called for type " << this->Type << std::endl;
+}
+
+AMateria &AMateria::operator=(const AMateria &other)
+{
+    std::cout << "AMateria: copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->Type = other.Type;
+    return *this;
+}
+
+std::string const & AMateria::getType() const
+{
+    return (this->Type);
+}
+
+void AMateria::setType(std::string type)
+{
+    this->Type = type;
+}
+
+void AMateria::use(ICharacter& target)
+{
+    std::cout << "calling the use method from the AMateria" << std::endl;
+}
---
+++ cpp04/ex04e/main.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include "AMateria.hpp"
+
+
+int main()
+{
+
+    // AMateria *first = new AMateria(); we can't creat object from in abstract class
+
+
+    
+
+
+
+
+
+
+}
